b065edf95ea1dfd59915cf53bad72347b1113943
rodrigomaldonadov <rodrigomaldonadov8@gmail.com>
2026-05-14 10:39:52 -0500

    first init

---
+++ src/main/java/com/rodrigomv/planetbooksback/PlanetbooksBackApplication.java
@@ -0,0 +1,13 @@
+package com.rodrigomv.planetbooksback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanetbooksBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlanetbooksBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rodrigomv/planetbooksback/PlanetbooksBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodrigomv.planetbooksback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanetbooksBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
