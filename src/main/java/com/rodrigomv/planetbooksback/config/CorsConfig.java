8d2eaf78ba747f4944ff662aae59a3ad8c628eda
MAX-0-1 <as5865577@gmail.com>
2026-06-02 07:18:00 -0500

    Update CORS configuration to allow multiple origins and enable credentials

--- src/main/java/com/rodrigomv/planetbooksback/config/CorsConfig.java
+++ src/main/java/com/rodrigomv/planetbooksback/config/CorsConfig.java
@@ -14,10 +14,14 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                    .allowedOrigins("http://localhost:3000")
+                    .allowedOrigins(
+                        "http://localhost:3000",                  
+                        "https://planetbooks-front.onrender.com",
+                        "https://planetbook.solidwebs.com"        
+                    )
                     .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
                     .allowedHeaders("*")
-                    .allowCredentials(false);
+                    .allowCredentials(true); 
             }
         };
     }
